1dca33e044957ba60b6fd50f2791199553226f60
El Roble <san_lucas2005@yahoo.com>
2021-07-21 12:27:18 -0600

    Add project component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import { Navbar } from "react-bootstrap"
+import Navigation from './Navigation'
 
 export default function Header() {
     return (
         <header>
         <Navbar bg="dark" variant="dark">
-      <Navbar.Brand href="#home">
-      Jorge Soto
-      </Navbar.Brand>
-  </Navbar> 
+            <Navbar.Brand href="#home">
+                Jorge Soto
+            </Navbar.Brand>
+            <Navigation/>
+        </Navbar> 
         </header>
     )
 }
