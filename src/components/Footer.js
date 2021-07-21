2a987474b8ac7619e469291a5eaa38cb15c7c56f
El Roble <san_lucas2005@yahoo.com>
2021-07-21 11:45:50 -0600

    Add Header and Footer components, style.css file

---
+++ src/components/Footer.js
@@ -0,0 +1,22 @@
+import React from "react"
+import "../css/style.css"
+
+const Footer = () => {
+  return (
+    <>
+    <footer>
+
+        <div class="social-links container">
+            <a href="https://www.linkedin.com/in/jorge-soto-at-cabo/" target="_blank" rel="noreferrer"><i class="fab fa-linkedin fa-2x"></i></a>
+            <a href="https://github.com/jorgeatcabo" target="_blank" rel="noreferrer"><i class="fab fa-github fa-2x"></i></a>
+            <a href="https://stackexchange.com/users/20524588/jorgesoto" target="_blank" rel="noreferrer"><i class="fab fa-stack-overflow fa-2x"></i></a>
+        </div>
+
+    
+    </footer>
+    </>
+    
+  )
+}
+
+export default Footer
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Navbar } from "react-bootstrap"
+
+export default function Header() {
+    return (
+        <header>
+        <Navbar bg="dark" variant="dark">
+      <Navbar.Brand href="#home">
+      Jorge Soto
+      </Navbar.Brand>
+  </Navbar> 
+        </header>
+    )
+}
