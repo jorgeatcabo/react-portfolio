be72075e2274fcd832310dac62d96508c778b642
El Roble <san_lucas2005@yahoo.com>
2021-07-24 08:33:52 -0600

    Add developer's proficiencies to Resume page

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -24,7 +24,16 @@ export default function AboutMe() {
     <Card.Body>
       <Card.Title>Front End</Card.Title>
       <Card.Text>
-      
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>Javascript</li>
+          <li>JQuery</li>
+          <li>React JS</li>
+          <li>Bootstrap</li>
+          <li>Flexbox</li>
+          <li>Handlebars</li>
+        </ul>
       </Card.Text>
     </Card.Body>
   </Card>
@@ -33,7 +42,16 @@ export default function AboutMe() {
     <Card.Body>
       <Card.Title>Backend End</Card.Title>
       <Card.Text>
-      
+      <ul>
+          <li>Node Js</li>
+          <li>Express</li>
+          <li>Sequelize</li>
+          <li>MySQL</li>
+          <li>MVC Paradigm</li>
+          <li>Heroku</li>
+          <li>AWS</li>
+          <li>Linux</li>
+        </ul>
       </Card.Text>
     </Card.Body>
   </Card>
