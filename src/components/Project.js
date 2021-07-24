2914a8d05801c9f71db95a65cd9fd6b76f87ae38
El Roble <san_lucas2005@yahoo.com>
2021-07-24 10:59:13 -0600

    Add tooltips to protfolio page

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import "../style.css"
-import { Col,Row,Card } from "react-bootstrap"
+import { Col,Row,Card,OverlayTrigger,Tooltip } from "react-bootstrap"
 
 
 function Project({project}) {
@@ -8,15 +8,33 @@ function Project({project}) {
         width: '18rem',
       };
 
+      const renderDeployTooltip = props => (
+        <Tooltip {...props}>Enjoy The App!</Tooltip>
+      );
+
+      const renderGithubTooltip = props => (
+        <Tooltip {...props}>Enjoy The Code!</Tooltip>
+      );
+
     return (
       <>
+    
       <Row className="card-row">
       <Col>
      <Card border="dark">
-        <Card.Header>{`${project.title}`}</Card.Header>
+        <Card.Header>{`${project.title}`}
+        <br />
+        <OverlayTrigger placement="top" overlay={renderDeployTooltip}>
+            <a href={`${project.link}`} target="_blank" rel="noreferrer">App Working</a>
+        </OverlayTrigger>
+        <br />
+        <OverlayTrigger placement="top" overlay={renderGithubTooltip}>
+            <a href={`${project.githubRepo}`} target="_blank" rel="noreferrer">GitHub Repo</a>
+        </OverlayTrigger>
+        </Card.Header>
         <Card.Body>
           <Card.Text>
-            <a href={`${project.link}`} target="_blank" rel="noreferrer"><img src={`${project.image}`} alt={`${project.title}`} style={cardStyle} /></a>
+             <a href={`${project.link}`} target="_blank" rel="noreferrer"><img src={`${project.image}`} alt={`${project.title}`} style={cardStyle} /></a>
           </Card.Text>
         </Card.Body>
       </Card>
--- src/projects.js
+++ src/projects.js
@@ -4,36 +4,42 @@ const projects = [
     title: 'Happy Pets App',
     image: '/assets/images/user.gif',
     link:'https://desolate-tundra-25750.herokuapp.com/',
+    githubRepo:'https://github.com/adina-hc/happy-pets-shelter-app',
   },
   {   
     id:'2',
     title: 'The Tech Blog App',
     image: '/assets/images/tech-blog-screenshot.PNG',
     link:'https://salty-taiga-28543.herokuapp.com/',
+    githubRepo:'https://github.com/jorgeatcabo/tech-blog',
   },
   {   
     id:'3',
     title: 'Note Taker App',
     image: '/assets/images/note-taker-1.PNG',
     link:'https://pure-thicket-39065.herokuapp.com/',
+    githubRepo:'https://github.com/jorgeatcabo/note-taker',
   },
   {   
     id:'4',
     title: 'Vagabundos App',
     image: '/assets/images/VagabundosApp.gif',
     link:'https://jorgeatcabo.github.io/vagabundos-application/',
+    githubRepo:'https://github.com/jorgeatcabo/vagabundos-app',
   },
   {   
     id:'5',
     title: 'Password Generator App',
     image: '/assets/images/password-generator-image.png',
     link:'https://jorgeatcabo.github.io/password-generator/',
+    githubRepo:'https://github.com/jorgeatcabo/password-generator',
   },
   {   
     id:'6',
     title: 'Work Day Scheduler App',
     image: '/assets/images/work-day-scheduler.png',
     link:'https://jorgeatcabo.github.io/work-day-scheduler/',
+    githubRepo:'https://github.com/jorgeatcabo/work-day-scheduler',
   },
   
 ];
