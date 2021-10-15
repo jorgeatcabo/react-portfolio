97ae6b940b05e1d98333ee4552b6080f36cee433
El Roble <san_lucas2005@yahoo.com>
2021-10-14 19:29:16 -0600

    Add pata de perro project

--- src/projects.js
+++ src/projects.js
@@ -4,45 +4,54 @@ import NoteTakerApp from './assets/images/note-taker-1.PNG'
 import VagabundosApp from './assets/images/VagabundosApp.gif'
 import PasswordGeneratorApp from './assets/images/password-generator-image.png'
 import WorkDaySchedulerApp from './assets/images/work-day-scheduler.png'
+import PataDePerroApp from './assets/images/pata-de-perro3.gif'
+
 
 const projects = [
   {   
     id:'1',
+    title: 'Pata De Perro App',
+    image: PataDePerroApp,
+    link:'https://shrouded-bayou-22662.herokuapp.com/',
+    githubRepo:'https://github.com/adina-hc/pata-de-perro',
+  },
+  {   
+    id:'2',
     title: 'Happy Pets App',
     image: HappyPetsApp,
     link:'https://desolate-tundra-25750.herokuapp.com/',
     githubRepo:'https://github.com/adina-hc/happy-pets-shelter-app',
   },
   {   
-    id:'2',
+    id:'3',
     title: 'The Tech Blog App',
     image: TheTechBlogApp,
     link:'https://salty-taiga-28543.herokuapp.com/',
     githubRepo:'https://github.com/jorgeatcabo/tech-blog',
   },
   {   
-    id:'3',
+    id:'4',
     title: 'Note Taker App',
     image: NoteTakerApp,
     link:'https://pure-thicket-39065.herokuapp.com/',
     githubRepo:'https://github.com/jorgeatcabo/note-taker',
   },
   {   
-    id:'4',
+    id:'5',
     title: 'Vagabundos App',
     image: VagabundosApp,
     link:'https://jorgeatcabo.github.io/vagabundos-application/',
     githubRepo:'https://github.com/jorgeatcabo/vagabundos-app',
   },
   {   
-    id:'5',
+    id:'6',
     title: 'Password Generator App',
     image: PasswordGeneratorApp,
     link:'https://jorgeatcabo.github.io/password-generator/',
     githubRepo:'https://github.com/jorgeatcabo/password-generator',
   },
   {   
-    id:'6',
+    id:'7',
     title: 'Work Day Scheduler App',
     image: WorkDaySchedulerApp,
     link:'https://jorgeatcabo.github.io/work-day-scheduler/',
