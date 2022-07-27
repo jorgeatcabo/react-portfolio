b3e6e4ea0be07c3e32cd1317dbb54ca538696199
El Roble <san_lucas2005@yahoo.com>
2022-07-27 11:51:59 -0600

    Testing again Visual Pro

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,6 @@ import React from "react"
 import "../style.css"
 
 const Footer = () => {
-
   
   return (
     <>
@@ -14,8 +13,6 @@ const Footer = () => {
           <a class="social-links" href="https://github.com/jorgeatcabo" target="_blank" rel="noreferrer"><i class="fab fa-github fa-2x"></i></a>
 
           <a class="social-links" href="https://stackexchange.com/users/20524588/jorgesoto" target="_blank" rel="noreferrer"><i class="fab fa-stack-overflow fa-2x"></i></a>
-
-
 
     </footer>
     </>
